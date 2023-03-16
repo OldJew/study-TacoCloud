be25fb3fe5e172df494fedf49e8ee7e849e34cba
OldJew <kuryat1997@gmail.com>
2023-03-16 19:47:01 +0300

    added unversioned files

---
+++ tacocloud-authorization-server/src/test/java/ru/oldjew/tacocloud/authorizationServer/TacocloudAuthorizationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.oldjew.tacocloud.authorizationServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacocloudAuthorizationServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
